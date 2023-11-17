56615cc6a816bd2bbd02dfc0c92f3b451c4615b6
Lawrence <e0176571@u.nus.edu>
2023-11-18 00:21:58 +0800

    fix configs for intellisense

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,27 @@
-import vue from "@vitejs/plugin-vue";
-import laravel from "laravel-vite-plugin";
-import path from "path";
-import { defineConfig } from "vite";
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: "resources/js/app.js",
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            "@": path.resolve("resources/js"),
-            ziggy: path.resolve("vendor/tightenco/ziggy/src/js"),
+  plugins: [
+    laravel({
+      input: 'resources/js/app.js',
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
         },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve('resources/js'),
+      ziggy: path.resolve('vendor/tightenco/ziggy/src/js'),
     },
+  },
 });
